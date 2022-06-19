deed435606ded2c541fe428f11d3cb99ad6fb411
govinda chastwe <govindachastwe54@gmail.com>
2022-06-20 02:43:36 +0530

    react-admin

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Table from './apicall/Table';
+
+
+
+const App = () => {
+  return (
+    <div>
+        <Table />
+        
+    </div>
+  )
+}
+
+export default App;
